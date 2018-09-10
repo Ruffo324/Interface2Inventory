3d59d29366f9d3171322c09a2518cdd68d89f5fc
Christian Groothoff <ruffo324@gmail.com>
2018-09-10 21:10:52 +0200

    #10 Clearscreen update.

--- Console.lua
+++ Console.lua
@@ -49,10 +49,10 @@ local consoleHeight = 0 -- Automatic setting in Init()
 --@param[opt=term] monitor Monitor wich should be cleaned.
 function ClearScreen(monitor)
   monitor = monitor or term
-  if(monitor ~= term) then
-    monitor.clear()
-  else
+  if(monitor == term) then
     shell.run("clear")
+  else
+    monitor.clear()
   end
   --for i = 1, 255 do
   --  CursorToNetxLine(monitor)
